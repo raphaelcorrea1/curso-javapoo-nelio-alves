adf1e874a7a2e79ffa867b7f65513b246320ddf8
Raphael <recaydxcorrea@outlook.com>
2024-02-29 21:24:38 -0300

    Uploading remaining files from module 18

---
+++ mod18/aula233/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import devices.ComboDevice;
+import devices.ConcretPrinter;
+import devices.ConcretScanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		ConcretPrinter p = new ConcretPrinter("1080");
+		p.processDoc("My Letter");
+		p.print("My Letter");
+		
+        System.out.println();
+		ConcretScanner s = new ConcretScanner("2003");
+		s.processDoc("My Email");
+		System.out.println("Scan result: " + s.scan());
+
+        System.out.println();
+        ComboDevice c = new ComboDevice("2081");
+        c.processDoc("My dissertation");
+        c.print("My dissertation");
+        System.out.println("Scan result: " + c.scan());
+	}
+}
